be314235e30dc5cf21b31d4b6540025579bd694f
karunakar <karunakarramoju143@gmail.com>
2024-11-06 20:26:23 +0530

    swiggy clone project

---
+++ client/src/suby/api.js
@@ -0,0 +1,5 @@
+
+
+ //export const API_URL = "https://backend-nodejs-suby-r9vw.onrender.com"
+
+export const API_URL = "http://localhost:4000"
---
+++ client/src/suby/data.js
@@ -0,0 +1,31 @@
+
+export const itemData = [{
+     item_img: "/assects/item/Bonda.jpg"
+},
+{
+    item_img: "/assects/item/Dosa.jpg"
+},
+{
+    item_img: "/assects/item/Idli.jpg"
+},
+{
+    item_img: "/assects/item/Juice.jpg"
+},
+{
+    item_img: "/assects/item/Pancake.jpg"
+},
+{
+    item_img:"/assects/item/Paratha.jpg"
+},
+{
+    item_img: "/assects/item/Poha.jpg"
+},
+{
+    item_img: "/assects/item/Poori.jpg"
+},
+{
+    item_img: "/assects/item/Vada.jpg"
+},
+
+
+]
